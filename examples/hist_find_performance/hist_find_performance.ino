af6b3d300f08386b61310121bf090505796efdef
rob tillaart <rob.tillaart@gmail.com>
2021-11-03 16:26:02 +0100

    example

--- examples/hist_find_performance/hist_find_performance.ino
+++ examples/hist_find_performance/hist_find_performance.ino
@@ -4,14 +4,14 @@
 // VERSION: 0.1.0
 //    DATE: 2021-11-03
 //
-// PUPROSE: test histogram find performance
+// PUPROSE: indication histogram find performance 
 //
 
 #include "histogram.h"
 
-float b[1000];
+float b[100];
 
-Histogram hist(1000, b);
+Histogram hist(100, b);
 
 uint32_t start, duration;
 uint32_t lastTime = 0;
@@ -25,7 +25,7 @@ void setup()
   Serial.println(HISTOGRAM_LIB_VERSION);
 
   // fill boundary array
-  for (int i = 0; i < 1000; i++) b[i] = i * 10.0;
+  for (int i = 0; i < 100; i++) b[i] = i * 10.0;
 
   Serial.print("# buckets: ");
   Serial.println(hist.size());
@@ -61,16 +61,6 @@ void setup()
   delay(10);
 
   x = 7654;
-  start = micros();
-  y = hist.find(x);
-  duration = micros() - start;
-  Serial.print("Duration: ");
-  Serial.println(duration);
-  Serial.print("  Bucket: ");
-  Serial.println(y);
-  delay(10);
-
-  x = 20000;
   start = micros();
   y = hist.find(x);
   duration = micros() - start;
